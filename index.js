bb0e3fd3da69e3ec517372799b523c24d0f4da2c
Ezequiel Luci <lucieze02@icloud.com>
2024-07-04 11:06:13 -0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const button = document.getElementById("menu-toggle");
 
 // Al hacer clic en el botón del menú, alternar la clase "active"
 button.addEventListener("click", (event) => {
-  event.stopPropagation(); // Evita la propagación del evento al body
   menu.classList.toggle("active");
 });
 
@@ -12,12 +11,5 @@ document.addEventListener("click", (event) => {
   if (menu.classList.contains("active")) {
     menu.classList.remove("active");
     menu.classList.add("desactive");
-  }else{
-    menu.classList.remove("active");
   }
-});
-
-// Evita la propagación del clic dentro del menú
-menu.addEventListener("click", (event) => {
-  event.stopPropagation();
 });

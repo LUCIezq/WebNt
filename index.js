40b233e28540f6b200dcce1f4fbdae8a3a6e8305
Ezequiel Luci <lucieze02@icloud.com>
2024-07-04 01:52:38 -0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const menu = document.getElementById("nav-list");
 const button = document.getElementById("menu-toggle");
+const body = document.getElementById("body");
 
 button.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+body.addEventListener("click", () => {
   menu.classList.toggle("active");
 });

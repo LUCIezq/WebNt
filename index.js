cfbf1ebc1830f574c7401ba22266e314ae3cff7e
Ezequiel Luci <lucieze02@icloud.com>
2024-07-04 11:11:21 -0300

    update

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const button = document.getElementById("menu-toggle");
 
 // Al hacer clic en el botón del menú, alternar la clase "active"
 button.addEventListener("click", (event) => {
+  event.stopPropagation(); // Evita la propagación del evento al body
   menu.classList.toggle("active");
 });
 
-// Al hacer clic en el body, si el menú está activo, agregar la clase "desactive"
-document.addEventListener("click", (event) => {
+// Al hacer clic en el body, si el menú está activo, eliminar la clase "active"
+document.addEventListener("click", () => {
   if (menu.classList.contains("active")) {
     menu.classList.remove("active");
-    menu.classList.add("desactive");
   }
+});
+
+// Evita la propagación del clic dentro del menú
+menu.addEventListener("click", (event) => {
+  event.stopPropagation();
 });
